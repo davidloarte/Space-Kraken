161e24f84aeb3324a384f9163342a947a218f196
David <32900055+davidloarte@users.noreply.github.com>
2020-04-12 14:02:03 +0200

    intento de subida 2

---
+++ Assets/SampleScenes/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    //lo iniciamos como objeto del juego
+    public GameObject ship;
+
+    //añadimos la distancia a la que estara la camara
+    //vector 3 porque es en 3d
+    private Vector3 espacio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        espacio = transform.position - ship.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = ship.transform.position + espacio;
+    }
+}
---
+++ Assets/SampleScenes/Scripts/ShipMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    float vrotacion = 4f;
+    public Rigidbody rb;
+    public Renderer rend;
+
+    // para poder cambiar el color
+    public Color color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = this.GetComponent<Renderer>();
+        rend.material.color = color;
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotar();
+        movimiento();
+        controlDireccion();
+    }
+
+    public void rotar()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            rb.AddTorque(transform.forward * -1f, ForceMode.Force);
+        if (Input.GetKey(KeyCode.E))
+            rb.AddTorque(transform.forward * 1f, ForceMode.Force);
+    }
+
+    public void movimiento()
+    {
+        if (Input.GetKey(KeyCode.W))
+           rb.AddForce(Vector3.forward * 5f, ForceMode.Force);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.back * 5f, ForceMode.Force);
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left * 3f, ForceMode.Force);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right * 3f, ForceMode.Force);
+        if (Input.GetKey(KeyCode.Space))
+            rb.AddForce(Vector3.up * 3f, ForceMode.Force);
+        if (Input.GetKey(KeyCode.LeftShift))
+            rb.AddForce(Vector3.down * 3f, ForceMode.Force);
+    }
+
+    public void controlDireccion()
+    {
+
+
+
+
+        //Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //float puntoMedio = (transform.position - Camera.main.transform.position).magnitude * 0.5f;
+
+        //transform.LookAt(mouseRay.origin + mouseRay.direction * puntoMedio);
+    }
+}
