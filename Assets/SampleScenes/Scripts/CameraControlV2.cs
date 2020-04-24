c20805954989cfb771fc37f37e1565feb0448144
David <32900055+davidloarte@users.noreply.github.com>
2020-04-24 18:42:06 +0200

    cambio de cubo a avion + movimiento
    He cambiado el cubo por una nave y a demas estoy trabajando en el movimiento de la camara con el cursor

--- Assets/SampleScenes/Scripts/CameraControlV2.cs
+++ Assets/SampleScenes/Scripts/CameraControlV2.cs
@@ -17,13 +17,17 @@ public class CameraControlV2 : MonoBehaviour
     // usamos un late update para indicar que se va a actualizar un frame mas tarde
     void LateUpdate()
     {
-        distancia = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 2, Vector3.up) * distancia;
+        //descarto poner el eje z porque no se puede mover el mouse en el
+        distancia = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 2, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * 2, Vector3.forward) * distancia;
+        //distancia = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * 2, Vector3.forward) * distancia;
+        //distancia = Quaternion.AngleAxis(Input.GetAxis("Mouse Z") * 2, Vector3.right) * distancia;
 
         transform.position = jugador.transform.position + distancia;
         transform.LookAt(jugador.transform.position);
 
         // Usamos referencia para que nuestros controles no varien
-        Vector3 copiaRotacion = new Vector3(0, transform.eulerAngles.y, 0);
+        //Vector3 copiaRotacion = new Vector3(0, transform.eulerAngles.y, 0);
+        Vector3 copiaRotacion = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
         referencia.transform.eulerAngles = copiaRotacion;
 
     }
--- Assets/SampleScenes/Scripts/ShipMovementV2.cs
+++ Assets/SampleScenes/Scripts/ShipMovementV2.cs
@@ -19,7 +19,6 @@ public class ShipMovementV2 : MonoBehaviour
     {
         float moverHorizontal = Input.GetAxis("Horizontal");
         float moverVertical = Input.GetAxis("Vertical");
-
         
         if (rb.velocity.magnitude > velocidadMax)
         {
@@ -28,6 +27,11 @@ public class ShipMovementV2 : MonoBehaviour
 
         rb.AddForce(moverHorizontal * referencia.transform.right * velocidad);
         rb.AddForce(moverVertical * referencia.transform.forward * velocidad);
+        //para arriba y abajo hay no hay ejes predefinidos
+        if (Input.GetKey(KeyCode.Space))
+            rb.AddForce(referencia.transform.up * velocidad, ForceMode.Force);
+        if (Input.GetKey(KeyCode.LeftShift))
+            rb.AddForce(referencia.transform.up * (-velocidad), ForceMode.Force);
 
     }
 }
