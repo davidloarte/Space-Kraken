092e34d99741d89579e2ee15d60634f6be56e7ef
David <32900055+davidloarte@users.noreply.github.com>
2020-04-24 12:30:58 +0200

    Cambios en los controladores
    He realizado cambios para poder rotar la camara y que las direcciones tambien roten. Aun no terminado

---
+++ Assets/SampleScenes/Scripts/CameraControlV2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlV2 : MonoBehaviour
+{
+    public GameObject jugador;
+    public GameObject referencia;
+    private Vector3 distancia;
+
+    void Start()
+    {
+        // esto hace que la disctancia con el jugador sea contante
+        distancia = transform.position - jugador.transform.position;        
+    }
+
+    // usamos un late update para indicar que se va a actualizar un frame mas tarde
+    void LateUpdate()
+    {
+        distancia = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 2, Vector3.up) * distancia;
+
+        transform.position = jugador.transform.position + distancia;
+        transform.LookAt(jugador.transform.position);
+
+        // Usamos referencia para que nuestros controles no varien
+        Vector3 copiaRotacion = new Vector3(0, transform.eulerAngles.y, 0);
+        referencia.transform.eulerAngles = copiaRotacion;
+
+    }
+}
---
+++ Assets/SampleScenes/Scripts/ShipMovementV2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovementV2 : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float velocidad;
+    public float velocidadMax;
+    public GameObject referencia;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moverHorizontal = Input.GetAxis("Horizontal");
+        float moverVertical = Input.GetAxis("Vertical");
+
+        
+        if (rb.velocity.magnitude > velocidadMax)
+        {
+            rb.velocity = rb.velocity.normalized * velocidadMax;
+        }
+
+        rb.AddForce(moverHorizontal * referencia.transform.right * velocidad);
+        rb.AddForce(moverVertical * referencia.transform.forward * velocidad);
+
+    }
+}
