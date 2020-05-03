cc366c281874fc777a4246dcb8ffeef4c43ecd32
David <32900055+davidloarte@users.noreply.github.com>
2020-05-03 19:11:25 +0200

    Mejora
    Una mejora en disparos, añadido musica y fondo y comenzado con las colisiones

---
+++ Assets/SampleScenes/Scripts/DestroyCollition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCollition : MonoBehaviour
+{
+    public GameObject ObjetoADestruir;
+    public Collider ObjetoSensible;
+
+    private void DetectaColision (Collider colision)
+    {
+        if (colision.gameObject.tag == "any" || colision.gameObject.tag == "asteroide")
+        {
+            Destroy(ObjetoADestruir);
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        DetectaColision(ObjetoSensible);
+        
+    }
+}
