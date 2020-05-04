6463e1a9991f45d8ebb82ae8155c0b9c639d2f94
David <32900055+davidloarte@users.noreply.github.com>
2020-05-04 17:56:24 +0200

    Propulsores añadidos
    Propulsores añadidos y funcionando

---
+++ Assets/SampleScenes/Scripts/PropulsorActivation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropulsorActivation : MonoBehaviour
+{
+    public ParticleSystem LanzadorParticulas;
+    void Start()
+    {
+        LanzadorParticulas = GetComponent<ParticleSystem>();
+        LanzadorParticulas.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            LanzadorParticulas.Play();
+        }
+        if (Input.GetKeyUp("w"))
+        {
+            LanzadorParticulas.Stop();
+        }
+
+    }
+}
