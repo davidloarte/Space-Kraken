4680255683773fa1257d1449064e17fc60e4c1c1
David <32900055+davidloarte@users.noreply.github.com>
2020-05-06 19:06:29 +0200

    Trabajando en explosiones

--- Assets/SampleScenes/Scripts/DestroyCollition.cs
+++ Assets/SampleScenes/Scripts/DestroyCollition.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class DestroyCollition : MonoBehaviour
 {
-
     public AudioClip sonidoExplosion;
     public float volumen;
     private Transform posicionSonido;
 
+    public GameObject efectoExplosion;
+
+    private bool haExplotado = false;
     void Start()
     {
         posicionSonido = transform;
@@ -26,15 +28,24 @@ public class DestroyCollition : MonoBehaviour
         Destroy(gameObject);
 
         // Dos maneras diferentes de decir para el mismo tag
-        if (collision.gameObject.tag.Equals ("asteroide1") || collision.gameObject.tag == "asteroide")
+        if ((collision.gameObject.tag.Equals ("asteroide1") || collision.gameObject.tag == "asteroide") && haExplotado == false)
         {
             //Si detecta la colision con un objeto de ese tio elimina el objeto y el disparo
             Destroy(collision.gameObject);
             Destroy(gameObject);
 
+            //Instanciamos el efecto con nuestro efecto, donde y su rotacion
+            Explotar();
+            haExplotado = true;
+
             // Hacemos que suene la explosion
             AudioSource.PlayClipAtPoint(sonidoExplosion, posicionSonido.position, volumen * 10000000);
         }
 
+    }
+
+    public void Explotar()
+    {
+        Instantiate(efectoExplosion, transform.position, transform.rotation);
     }
 }
