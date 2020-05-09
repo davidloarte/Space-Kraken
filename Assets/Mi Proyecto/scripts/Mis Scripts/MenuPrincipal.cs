b6859bcbb166492ac7076a9adf85611c9165d38f
David <32900055+davidloarte@users.noreply.github.com>
2020-05-09 19:42:56 +0200

    Mas Menu
    Trabajando con el menu

---
+++ Assets/Mi Proyecto/scripts/Mis Scripts/MenuPrincipal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Añadimos la libreria que nos permite manejar escenas
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void PlayScene()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
