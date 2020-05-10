b40529920ac99f8cde455afc600fd3fd4a0f99db
David <32900055+davidloarte@users.noreply.github.com>
2020-05-10 19:36:50 +0200

    Mas Menu
    Trabajando en animaciones para los menus

--- Assets/Mi Proyecto/scripts/Mis Scripts/MenuPrincipal.cs
+++ Assets/Mi Proyecto/scripts/Mis Scripts/MenuPrincipal.cs
@@ -3,9 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 //Añadimos la libreria que nos permite manejar escenas
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuPrincipal : MonoBehaviour
 {
+    public GameObject Menu;
+    public GameObject SaberPanel;
+
+    private Animator menuAnim;
+    private Animator saberPanelAnim;
+
+    private void Awake()
+    {
+        menuAnim = Menu.GetComponent<Animator>();
+        saberPanelAnim = SaberPanel.GetComponent<Animator>();
+    }
+
+    public void SaberMas()
+    {
+        SaberPanel.SetActive(true);
+
+        menuAnim.SetBool("Close", true);
+        saberPanelAnim.SetBool("Open", true);
+
+    }
+
     public void PlayScene()
     {
         SceneManager.LoadScene("Main Scene");
